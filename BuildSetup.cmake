63632ccf11f11692fe984a319ce4221c59cfbe4a
John Marshall <marshall@hep.phy.cam.ac.uk>
2009-09-10 14:32:58 +0000

    Added BuildSetup.cmake
    git-svn-id: https://svnsrv.desy.de/basic/PandoraPFANew/PandoraMonitoring/trunk@94 e861a63a-b6b5-47dc-a5de-b4304083765c

---
+++ BuildSetup.cmake
@@ -0,0 +1,55 @@
+#############################################################################
+# cmake build setup for PandoraMonitoring
+#
+# For building PandoraMonitoring with cmake type:
+# (1) $ mkdir build
+# (2) $ cd build
+# (3) $ cmake -C ../BuildSetup.cmake ..
+# (4) $ make install
+#
+# @author Jan Engels, DESY
+#############################################################################
+
+
+#############################################################################
+# Setup path variables
+#############################################################################
+
+# ILC_HOME
+SET( ILC_HOME "path_to_ilc_home"
+    CACHE PATH "Path to ILC Software" FORCE )
+
+# Path to LCIO
+SET( LCIO_HOME "${ILC_HOME}/lcio/v01-11"
+    CACHE PATH "Path to LCIO" FORCE )
+
+# Path to GEAR
+SET( GEAR_HOME "${ILC_HOME}/gear/v00-11-01"
+    CACHE PATH "Path to GEAR" FORCE )
+
+# Path to ROOT
+SET( ROOT_HOME "${ILC_HOME}/root/5.16.00"
+     CACHE PATH "Path to ROOT" FORCE )
+
+# CMake Modules Path
+SET( CMAKE_MODULE_PATH "${ILC_HOME}/CMakeModules/v01-08"
+    CACHE PATH "Path to CMake Modules" FORCE )
+
+###############################################
+# Project Options
+###############################################
+
+#SET( INSTALL_DOC OFF CACHE BOOL "Set to OFF to skip build/install Documentation" FORCE )
+
+# set cmake build type
+# possible options are: None Debug Release RelWithDebInfo MinSizeRel
+#SET( CMAKE_BUILD_TYPE "Debug" CACHE STRING "Choose the type of build" FORCE )
+
+###############################################
+# Advanced Options
+###############################################
+
+#SET( BUILD_SHARED_LIBS OFF CACHE BOOL "Set to OFF to build static libraries" FORCE )
+
+# installation path for PandoraPFA
+#SET( CMAKE_INSTALL_PREFIX "foo/bar" CACHE STRING "Where to install PandoraMonitoring" FORCE )
