8845054450025824903ab2d2e419dacb6a87eacc
John Marshall <marshall@hep.phy.cam.ac.uk>
2009-09-16 16:52:58 +0000

    Update to reflect change in return type for Cluster::GetOrderedCaloHitList()
    git-svn-id: https://svnsrv.desy.de/basic/PandoraPFANew/PandoraMonitoring/trunk@123 e861a63a-b6b5-47dc-a5de-b4304083765c

--- src/PandoraMonitoring.cc
+++ src/PandoraMonitoring.cc
@@ -67,9 +67,9 @@ void PandoraMonitoring::LookAtClusters(const pandora::ClusterList *const pCluste
     {
         std::cout << "Monitoring, Cluster! " << *clusterIter << std::endl;
 
-        const pandora::OrderedCaloHitList *const pOrderedCaloHitList((*clusterIter)->GetOrderedCaloHitList());
+        const pandora::OrderedCaloHitList orderedCaloHitList((*clusterIter)->GetOrderedCaloHitList());
 
-        for (pandora::OrderedCaloHitList::const_iterator iter = pOrderedCaloHitList->begin(), iterEnd = pOrderedCaloHitList->end(); iter != iterEnd; ++iter)
+        for (pandora::OrderedCaloHitList::const_iterator iter = orderedCaloHitList.begin(), iterEnd = orderedCaloHitList.end(); iter != iterEnd; ++iter)
         {
             for (pandora::CaloHitList::const_iterator caloHitIter = iter->second->begin(), caloHitIterEnd = iter->second->end(); caloHitIter != caloHitIterEnd; ++caloHitIter)
             {
