a897adee146ebd907daa3f60164b63bb1d9da765
John Marshall <marshall@hep.phy.cam.ac.uk>
2010-10-04 16:12:34 +0000

    Cosmetic changes.
    git-svn-id: https://svnsrv.desy.de/basic/PandoraPFANew/PandoraMonitoring/trunk@793 e861a63a-b6b5-47dc-a5de-b4304083765c

--- src/PandoraMonitoringApi.cc
+++ src/PandoraMonitoringApi.cc
@@ -41,24 +41,21 @@ void PandoraMonitoringApi::Fill2DHistogram(const std::string &name, float xValue
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
-void PandoraMonitoringApi::AddHistograms(const std::string &nameHisto0, const std::string &nameHisto1, 
-                                         double coeff0, double coeff1)
+void PandoraMonitoringApi::AddHistograms(const std::string &nameHisto0, const std::string &nameHisto1, double coeff0, double coeff1)
 {
     PandoraMonitoring::GetInstance()->AddMultiplyOrDivideHistograms(nameHisto0, nameHisto1, coeff0, coeff1, true, false );
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
-void PandoraMonitoringApi::MultiplyHistograms(const std::string &nameHisto0, const std::string &nameHisto1, 
-                                              double coeff0, double coeff1)
+void PandoraMonitoringApi::MultiplyHistograms(const std::string &nameHisto0, const std::string &nameHisto1, double coeff0, double coeff1)
 {
     PandoraMonitoring::GetInstance()->AddMultiplyOrDivideHistograms(nameHisto0, nameHisto1, coeff0, coeff1, false, true);
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
-void PandoraMonitoringApi::DivideHistograms(const std::string &nameHisto0, const std::string &nameHisto1, 
-                                            double coeff0, double coeff1)
+void PandoraMonitoringApi::DivideHistograms(const std::string &nameHisto0, const std::string &nameHisto1, double coeff0, double coeff1)
 {
     PandoraMonitoring::GetInstance()->AddMultiplyOrDivideHistograms(nameHisto0, nameHisto1, coeff0, coeff1, false, false);
 }
